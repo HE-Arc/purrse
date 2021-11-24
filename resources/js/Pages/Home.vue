96dc395a685f573ef01a95b741fc0cbb03e530b8
Tino3210 <izzovalentino0@gmail.com>
2021-11-24 22:17:58 +0100

    feat<#6>: add Home page vue.js
    - add the header div
    - add the description div
    - add the footer
    - style

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <Head title='Home' />
+    <div class='flex flex-col px-60 pt-10 bg-gray-700 text-gray-50'>
+        <div class='flex w-full min-h-40 justify-end bg-gray-500'>
+            <Link :href="route('login')" class="text-sm underline">
+                Log in
+            </Link>
+
+            <Link :href="route('register')" class="ml-4 text-sm underline">
+                Register
+            </Link>
+        </div>
+        <div class='h-full mt-36 bg-indigo-700'>
+            <h1 class='text-6xl font-normal leading-normal '>
+                Purrse
+            </h1>
+            <p class='mt-20'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam turpis, ullamcorper sit amet odio sit amet,
+                ltrices dictum elit. Nunc at enim a elit bibendum rhoncus eget sit amet risus. Suspendisse at ipsum sit amet ligula ullamcorper accumsan. Suspendisse gravida massa mi, et efficitur est ullamcorper porttitor. Fusce nulla metus, ultrices at condimentum a,
+                molestie mattis lacus. Quisque tincidunt ante non nisi pellentesque,
+                et scelerisque augue iaculis. Cras mi neque, condimentum eget dictum eget, rhoncus vitae dolor. Nam eu ante euismod nisi aliquam malesuada. Morbi mollis quis augue sit amet fringilla.
+            </p>
+        </div>
+        <footer class='bg-indigo-300'>
+            He-arc Neuchâtel
+        </footer>
+    </diV>
+</template>
+
+<script>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+export default {
+    components: {
+      Head,
+      Link,
+    },
+}
+</script>
