546934587395d294667b78f19100d877cec8afb0
Lopes Da Silva Diogo <diogo.lopesdasilva@he-arc.ch>
2022-01-05 18:10:06 +0100

    petite couille dans le paté

--- resources/js/Components/Space.vue
+++ resources/js/Components/Space.vue
@@ -9,7 +9,6 @@
                 <img src="../icons/chevron-down-solid.svg" alt="Sort Down" class="chevron"/>
             </object>
         </div>
-        -->
     </li>
     <transition name="fade">
         <SpaceModal v-if="showModal" @close="closeModal" :description="description" :budget="budget" :total="total" :to_pay="to_pay"/>
