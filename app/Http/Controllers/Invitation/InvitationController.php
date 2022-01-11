ce66bbd00919316fd9c02ee097f72665ee1ac3e7
Tino3210 <izzovalentino0@gmail.com>
2022-01-11 22:15:05 +0100

    fix: invitation accept

--- app/Http/Controllers/Invitation/InvitationController.php
+++ app/Http/Controllers/Invitation/InvitationController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Invitation;
 
 use App\Http\Controllers\Controller;
@@ -23,12 +24,12 @@ class InvitationController extends Controller
         $exist = false;
         $error = false;
         $listName = null;
-        if(sizeof($arrayLists) == 1){ //If one list is find
+        if (sizeof($arrayLists) == 1) { //If one list is find
             $listName = $arrayLists[0]->name; //retrives the name
             //Find in the rellation table user_to_list, if the user has already acces to the list
-            $userToList = UserToList::where([['user_id', Auth::id()],['list_id', $arrayLists[0]->id]])->get();
+            $userToList = UserToList::where([['user_id', Auth::id()], ['list_id', $arrayLists[0]->id]])->get();
             $exist = sizeof($userToList) > 0 ? true : false;
-        }else{
+        } else {
             $error = true; //Error when the token refere to any list
         }
 
@@ -51,12 +52,12 @@ class InvitationController extends Controller
         $arrayListId = Lists::where('token', $request->route('token'))->get('id');
 
         //add the list to the relation table user_to_list
-        if(sizeof($arrayListId) == 1){
+        if (sizeof($arrayListId) == 1) {
             $userToList = UserToList::firstOrCreate([
                 'user_id' => Auth::id(),
                 'list_id' => $arrayListId[0]->id,
             ]);
         }
-        return Inertia::render('Application/Mainboard'); //Go to the home page
+        return Inertia::render('Application/Home'); //Go to the home page
     }
 }
